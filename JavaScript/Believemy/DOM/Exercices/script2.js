801c1309d82a8d4d085f91d59fa523eb17d6da59
Jossic <jossic.lapierre@gmail.com>
2020-03-26 22:23:13 +0100

    Fin journée du 26

--- JavaScript/Believemy/DOM/Exercices/script2.js
+++ JavaScript/Believemy/DOM/Exercices/script2.js
@@ -1,8 +1,93 @@
-function somme(nb) {
-  if (nb == 1) {
-    return 1;
-  }
-  return nb + somme(nb - 1);
-}
+// function somme(nb) {
+//   if (nb == 1) {
+//     return 1;
+//   }
+//   return nb + somme(nb - 1);
+// }
 
-console.log(somme(5));
+// console.log(somme(5));
+
+// let chien = {
+//   race: "shiba inu",
+//   poil: "court",
+//   // aboyer: () => console.log("waf waf")
+//   aboyer
+// };
+
+// chien.aboyer();
+
+// let info = ["Jossic", "Lapierre", "Comptable", "Développeur"];
+
+// let [prenom, nom, job1, job2] = info;
+
+// console.log(prenom);
+// console.log(nom);
+// console.log(job1);
+// console.log(job2);
+
+//les MAP
+
+// let coord = new Map([
+//   ["prenom", "Jossic"],
+//   ["nom", "Lapierre"],
+//   ["job1", "Comptable"]
+// ]);
+
+// coord.set("job2", "Développeur");
+// coord.delete("job1");
+
+// console.log(coord);
+
+// function add(...nb) {
+//   let res = 0;
+//   nb.forEach(nbr => {
+//     res += nbr;
+//   });
+//   console.log(res);
+// }
+
+// add(5, 10, 20, 65);
+
+// let btn = document.querySelector("button");
+// let interval;
+// let secondes = 5;
+
+// // btn.onclick = start();
+// btn.addEventListener("click", start);
+
+// function start() {
+//   interval = setInterval(decompte, 500);
+// }
+
+// function decompte() {
+//   secondes--;
+//   if (secondes == 0) {
+//     stop();
+//   } else {
+//     document.body.innerHTML += secondes + "<br>";
+//   }
+// }
+
+// function stop() {
+//   clearInterval(interval);
+//   document.body.innerHTML += "Stop !";
+// }
+
+// Spoiler
+let btn = document.querySelector("button");
+let p = document.querySelector("p");
+let hidden = false;
+
+p.style.display = "none";
+
+btn.addEventListener("click", () => {
+  if (!hidden) {
+    btn.textContent = "Cacher";
+    p.style.display = "contents";
+    hidden = true;
+  } else {
+    btn.textContent = "Afficher";
+    hidden = false;
+    p.style.display = "none";
+  }
+});
---
+++ JavaScript/Believemy/DOM/justePrix/script.js
@@ -0,0 +1,11 @@
+// Etape 1 - Sélectionner nos éléments
+
+// Etape 2 - Cacher l'erreur
+
+// Etape 3 - Générer un nombre aléatoire
+
+// Etape 4 - Vérifier que l'utilisateur donne bien un nombre
+
+// Etape 5 - Agir à l'envoi du formulaire
+
+// Etape 6 - Créer la fonction vérifier
