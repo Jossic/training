3291efd6300cd12d94795f18c43bc80852e16353
Jossic <jossic.lapierre@gmail.com>
2020-03-28 21:52:20 +0100

    fin Belivemy

--- JavaScript/Believemy/API/meteo/script.js
+++ JavaScript/Believemy/API/meteo/script.js
@@ -1,5 +1,37 @@
-let villeC = "Sommepy-Tahure";
-recevoirTemp(villeC);
+let villeC;
+// recevoirTemp(villeC);
+
+if ("geolocation" in navigator) {
+    navigator.geolocation.watchPosition((position) => {
+        const url = "http://api.openweathermap.org/data/2.5/weather?lon=" + position.coords.longitude + '&lat=' + position.coords.latitude + "&appid=593934f72669afa98c34ac1c17718026&units=metric";
+        let requete = new XMLHttpRequest();
+        requete.open('GET', url);
+        requete.responseType = 'json';
+        requete.send();
+        requete.onload = function () {
+            if (requete.readyState === XMLHttpRequest.DONE) {
+                if (requete.status === 200) {
+                    let reponse = requete.response;
+                    let temp = reponse.main.temp;
+                    let ville = reponse.name;
+                    document.querySelector('#temperature_label').textContent = temp;
+                    document.querySelector('#ville').textContent = ville;
+                }
+                else {
+                    alert('Un problème est survenu, merci de revenir plus tard');
+                }
+            }
+        }
+    }, erreur, options);
+
+    var options = {
+        enableHighAccuracy: true
+    }
+}
+else {
+    villeC = "Epernay";
+    recevoirTemp(villeC);
+}
 
 
 
@@ -11,6 +43,13 @@ btn.addEventListener('click', () => {
     recevoirTemp(villeC);
 
 });
+
+function erreur() {
+    villeC = "Epernay";
+    recevoirTemp = villeC;
+}
+
+
 function recevoirTemp(ville) {
     const url = "http://api.openweathermap.org/data/2.5/weather?q=" + ville + "&appid=593934f72669afa98c34ac1c17718026&units=metric";
     let requete = new XMLHttpRequest();
