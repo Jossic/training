99d51973df8484fca8282ab90025d977ce401e54
Jossic <jossic.lapierre@gmail.com>
2020-04-08 15:50:07 +0200

    fin movie fight

--- JavaScript/Colt Steele/movieFight/index.js
+++ JavaScript/Colt Steele/movieFight/index.js
@@ -63,27 +63,45 @@ const onMovieSelect = async (movie, summaryElement, side) => {
     if (leftMovie && rightMovie) {
         runComparison();
     }
-
 };
 
 const runComparison = () => {
+    const leftSideStats = document.querySelectorAll(
+        '#left-summary .notification'
+    );
+    const rightSideStats = document.querySelectorAll(
+        '#right-summary .notification'
+    );
 
+    leftSideStats.forEach((leftStat, index) => {
+        const rightStat = rightSideStats[index];
+
+        const leftSideValue = parseInt(leftStat.dataset.value);
+        const rightSideValue = parseInt(rightStat.dataset.value);
+
+        if (rightSideValue > leftSideValue) {
+            leftStat.classList.remove('is-primary');
+            leftStat.classList.add('is-warning');
+        } else {
+            rightStat.classList.remove('is-primary');
+            rightStat.classList.add('is-warning');
+        }
+    });
 };
 
 const movieTemplate = movieDetail => {
-    const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, ''));
+    const dollars = parseInt(
+        movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, '')
+    );
     const metascore = parseInt(movieDetail.Metascore);
     const imdbRating = parseFloat(movieDetail.imdbRating);
     const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''));
-
-    let count = 0;
-    const awards = movieDetail.Awards.split('').reduce((prev, word) => {
+    const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
         const value = parseInt(word);
 
         if (isNaN(value)) {
             return prev;
-        }
-        else {
+        } else {
             return prev + value;
         }
     }, 0);
@@ -104,25 +122,23 @@ const movieTemplate = movieDetail => {
         </div>  
     </article>
     
-    <article class= "notification is-primary">
+    <article data-value="${awards}" class="notification is-primary">
     <p class="title">${movieDetail.Awards}</p>
     <p class="subtitle">Awards</p>
     </article>
-    <article class= "notification is-primary">
+    <article data-value="${dollars}" class="notification is-primary">
     <p class="title">${movieDetail.BoxOffice}</p>
     <p class="subtitle">Box Office</p>
     </article>
-
-    
-    <article class= "notification is-primary">
+    <article data-value="${metascore}" class="notification is-primary">
     <p class="title">${movieDetail.Metascore}</p>
     <p class="subtitle">Metascore</p>
     </article>
-    <article class= "notification is-primary">
+    <article data-value="${imdbRating}" class="notification is-primary">
     <p class="title">${movieDetail.imdbRating}</p>
     <p class="subtitle">IMDB Rating</p>
     </article>
-    <article class= "notification is-primary">
+    <article data-value="${imdbVotes}" class="notification is-primary">
     <p class="title">${movieDetail.imdbVotes}</p>
     <p class="subtitle">IMDB Votes</p>
     </article>
