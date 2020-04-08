58d1275dc601bc206d8c9d2d8768f8fd9d9e2a9a
Jossic <jossic.lapierre@gmail.com>
2020-04-08 15:11:42 +0200

    fin 261

--- JavaScript/Colt Steele/movieFight/index.js
+++ JavaScript/Colt Steele/movieFight/index.js
@@ -30,7 +30,7 @@ createAutoComplete({
     root: document.querySelector('#left-autocomplete'),
     onOptionSelect(movie) {
         document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#left-summary'));
+        onMovieSelect(movie, document.querySelector('#left-summary'), 'left');
     }
 });
 createAutoComplete({
@@ -38,12 +38,13 @@ createAutoComplete({
     root: document.querySelector('#right-autocomplete'),
     onOptionSelect(movie) {
         document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#right-summary'));
+        onMovieSelect(movie, document.querySelector('#right-summary'), 'right');
     }
 });
 
-
-const onMovieSelect = async (movie, summaryElement) => {
+let leftMovie;
+let rightMovie;
+const onMovieSelect = async (movie, summaryElement, side) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: 'cf8af582',
@@ -52,9 +53,41 @@ const onMovieSelect = async (movie, summaryElement) => {
     });
 
     summaryElement.innerHTML = movieTemplate(response.data);
+
+    if (side === 'left') {
+        leftMovie = response.data;
+    } else {
+        rightMovie = response.data;
+    }
+
+    if (leftMovie && rightMovie) {
+        runComparison();
+    }
+
+};
+
+const runComparison = () => {
+
 };
 
 const movieTemplate = movieDetail => {
+    const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, ''));
+    const metascore = parseInt(movieDetail.Metascore);
+    const imdbRating = parseFloat(movieDetail.imdbRating);
+    const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''));
+
+    let count = 0;
+    const awards = movieDetail.Awards.split('').reduce((prev, word) => {
+        const value = parseInt(word);
+
+        if (isNaN(value)) {
+            return prev;
+        }
+        else {
+            return prev + value;
+        }
+    }, 0);
+
     return `
     <article class="media">
         <figure class="media-left">
@@ -70,6 +103,7 @@ const movieTemplate = movieDetail => {
             </div>
         </div>  
     </article>
+    
     <article class= "notification is-primary">
     <p class="title">${movieDetail.Awards}</p>
     <p class="subtitle">Awards</p>
@@ -78,6 +112,8 @@ const movieTemplate = movieDetail => {
     <p class="title">${movieDetail.BoxOffice}</p>
     <p class="subtitle">Box Office</p>
     </article>
+
+    
     <article class= "notification is-primary">
     <p class="title">${movieDetail.Metascore}</p>
     <p class="subtitle">Metascore</p>
