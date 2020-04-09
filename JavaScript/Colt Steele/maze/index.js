3a9de0c9910aaa27cb3303829dd3984b67726aea
Jossic <jossic.lapierre@gmail.com>
2020-04-09 21:52:25 +0200

    fin 284

--- JavaScript/Colt Steele/maze/index.js
+++ JavaScript/Colt Steele/maze/index.js
@@ -1,5 +1,7 @@
+
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -28,3 +30,56 @@ const walls = [
 ];
 World.add(world, walls);
 
+
+// Mise en place du laby
+
+const shuffle = (arr) => {
+    let counter = arr.length;
+
+    while (counter > 0) {
+        const index = Math.floor(Math.random() * counter);
+        counter--;
+        const temp = arr[counter];
+        arr[counter] = arr[index];
+        arr[index] = temp;
+    }
+}
+const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
+const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false));
+const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCells = (row, column) => {
+    if (grid[row][column]) {
+        return;
+    }
+
+    grid[row][column] = true;
+
+    const neighbors = shuffle([
+        [row - 1, column, 'up'],
+        [row, column + 1, 'right'],
+        [row + 1, column, 'down'],
+        [row, column - 1, 'left']
+    ]);
+
+    for (const neighbor of neighbors) {
+        const [nextRow, nextColumn, direction] = neighbor;
+        if (nextRow < 0 || nextRow >= cells || nextcolumn < 0 || nextColumn >= cells) {
+            continue;
+        }
+        if (grid[nextRow][nextColumn]) {
+            continue;
+        }
+
+
+
+
+    }
+
+
+};
+
+stepThroughCells(startRow, startColumn)
