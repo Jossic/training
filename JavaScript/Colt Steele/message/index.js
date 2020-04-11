25a40620a7a6211a2e97de0ae6f542c94aaa439a
Jossic <jossic.lapierre@gmail.com>
2020-04-11 20:32:28 +0200

    message fin

--- JavaScript/Colt Steele/message/index.js
+++ JavaScript/Colt Steele/message/index.js
@@ -1,10 +1,24 @@
+const { hash } = window.location;
+
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#message-show').classList.remove('hide');
+
+    document.querySelector('h1').innerHTML = message;
+}
+
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
+
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
 
     const input = document.querySelector('#message-input');
     const encrypted = btoa(input.value);
 
     const linkinput = document.querySelector('#link-input');
-    linkinput.value = `${window.location}#encrypted`;
+    linkinput.value = `${window.location}#${encrypted}`;
     linkinput.select();
 });
