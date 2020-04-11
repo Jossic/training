97f66ddf356d1a21d8de5a765c3a639ecc1684ef
Jossic <jossic.lapierre@gmail.com>
2020-04-11 14:23:05 +0200

    fin 295

--- JavaScript/Colt Steele/maze/index.js
+++ JavaScript/Colt Steele/maze/index.js
@@ -1,5 +1,5 @@
 
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body } = Matter;
 
 const cells = 3;
 const width = 600;
@@ -8,6 +8,7 @@ const height = 600;
 const unitLenght = width / cells;
 
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
     element: document.body,
@@ -23,10 +24,10 @@ Runner.run(Runner.create(), engine);
 
 
 const walls = [
-    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+    Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 2, height, { isStatic: true })
 ];
 World.add(world, walls);
 
@@ -110,11 +111,77 @@ horizontals.forEach((row, rowIndex) => {
             columnIndex * unitLenght + unitLenght / 2,
             rowIndex * unitLenght + unitLenght,
             unitLenght,
-            10,
+            7,
             { isStatic: true }
         );
         World.add(world, wall);
     });
 
+    verticals.forEach((row, rowIndex) => {
+        row.forEach((open, columnIndex) => {
+            if (open) {
+                return;
+            }
+
+            const wall = Bodies.rectangle(
+                columnIndex * unitLenght + unitLenght,
+                rowIndex * unitLenght + unitLenght / 2,
+                7,
+                unitLenght,
+                { isStatic: true }
+            );
+            World.add(world, wall);
+        });
+    });
+
 
 });
+
+const goal = Bodies.rectangle(
+    width - unitLenght / 2,
+    height - unitLenght / 2,
+    unitLenght * 0.7,
+    unitLenght * 0.7,
+    {
+        label: 'goal',
+        isStatic: true
+    }
+
+);
+World.add(world, goal);
+
+const ball = Bodies.circle(
+    unitLenght / 2,
+    unitLenght / 2,
+    unitLenght / 4
+);
+
+World.add(world, ball);
+
+document.addEventListener('keydown', event => {
+    const { x, y } = ball.velocity;
+
+    if (event.keyCode === 37) {
+        Body.setVelocity(ball, { x: x - 5, y });
+    }
+    if (event.keyCode === 38) {
+        Body.setVelocity(ball, { x, y: y - 5 });
+    }
+    if (event.keyCode === 39) {
+        Body.setVelocity(ball, { x: x + 5, y });
+    }
+    if (event.keyCode === 40) {
+        Body.setVelocity(ball, { x, y: y + 5 });
+    }
+});
+
+
+Events.on(engine, 'collisionStart', event => {
+    event.pairs.forEach(collision => {
+        const labels = ['ball', 'goal'];
+
+        if (labels.includes(collision.bodyA.label) && labels.includes(collision.bodyB.label)) {
+            console.log("win");
+        }
+    })
+})
