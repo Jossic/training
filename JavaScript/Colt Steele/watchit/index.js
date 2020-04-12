d5121de7e7ad239eb25e5c8e9b6a6b70fff3cc0b
Jossic <jossic.lapierre@gmail.com>
2020-04-12 15:39:30 +0200

    fin 343

---
+++ JavaScript/Colt Steele/watchit/index.js
@@ -0,0 +1,14 @@
+
+
+const debounce = require('lodash.debounce');
+const chokidar = require('chokidar');
+const program = require('caporal');
+
+const start = debounce(() => {
+    console.log("STARTING USERS PROGRAM");
+}, 100);
+
+chokidar.watch('.')
+    .on('add', start)
+    .on('change', () => console.log('FILE CHANGED'))
+    .on('unlink', () => console.log('FILE UNLINKED'));
