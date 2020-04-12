b326b65095b1b45051a66d8ce49a46868fac03d0
Jossic <jossic.lapierre@gmail.com>
2020-04-12 21:55:35 +0200

    fin 349

--- JavaScript/Colt Steele/watchit/index.js
+++ JavaScript/Colt Steele/watchit/index.js
@@ -4,6 +4,7 @@ const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const program = require('caporal');
 const fs = require('fs');
+const { spawn } = require('child_process');
 
 
 
@@ -20,7 +21,7 @@ program
         }
 
         const start = debounce(() => {
-            console.log("STARTING USERS PROGRAM");
+            spawn('node', [name], { stdio: 'inherit' });
         }, 100);
 
         chokidar.watch('.')
