b14d8494d5af5fac25a542a813c5711910a59602
Jossic <jossic.lapierre@gmail.com>
2020-04-12 18:15:55 +0200

    fin 345

--- JavaScript/Colt Steele/watchit/index.js
+++ JavaScript/Colt Steele/watchit/index.js
@@ -3,12 +3,32 @@
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const program = require('caporal');
+const fs = require('fs');
 
-const start = debounce(() => {
-    console.log("STARTING USERS PROGRAM");
-}, 100);
 
-chokidar.watch('.')
-    .on('add', start)
-    .on('change', () => console.log('FILE CHANGED'))
-    .on('unlink', () => console.log('FILE UNLINKED'));
+
+program
+    .version('0.0.1')
+    .argument('[filename]', 'Name of a file to execute')
+    .action(async ({ filename }) => {
+        const name = filename || 'index.js';
+
+        try {
+            await fs.promises.access(name);
+        } catch (err) {
+            throw new Error(`Could not fint the file ${name}`);
+        }
+
+        const start = debounce(() => {
+            console.log("STARTING USERS PROGRAM");
+        }, 100);
+
+        chokidar.watch('.')
+            .on('add', start)
+            .on('change', () => console.log('FILE CHANGED'))
+            .on('unlink', () => console.log('FILE UNLINKED'));
+
+    });
+
+
+
