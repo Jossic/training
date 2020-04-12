78278caf83bee8d2ed2194d9e3f51da5d87c61cd
Jossic <jossic.lapierre@gmail.com>
2020-04-12 22:58:15 +0200

    fin 359

--- JavaScript/Colt Steele/ecomm/index.js
+++ JavaScript/Colt Steele/ecomm/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send(`
@@ -15,9 +18,14 @@ app.get('/', (req, res) => {
     `);
 });
 
+
+
 app.post('/', (req, res) => {
+    console.log(req.body);
     res.send('Account created');
 });
+
+
 
 app.listen(3000, () => {
     console.log('listening');
