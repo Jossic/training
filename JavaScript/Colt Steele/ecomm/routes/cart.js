20298ff029dc8fd7bd73de1390f9d67b0ba36295
Jossic <jossic.lapierre@gmail.com>
2020-04-17 17:47:31 +0200

    fin ecomm

--- JavaScript/Colt Steele/ecomm/routes/cart.js
+++ JavaScript/Colt Steele/ecomm/routes/cart.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cartsRepo = require('../repositories/cart');
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
 
@@ -26,9 +28,34 @@ router.post('/cart/products', async (req, res) => {
         items: cart.items
     });
 
-    res.send('Produit ajouté au panier');
+    res.redirect('/cart');
 });
 
+router.get('/cart', async (req, res) => {
+    if (!req.session.cartId) {
+        return res.redirect('/');
+    }
 
+    const cart = await cartsRepo.getOne(req.session.cartId);
+
+    for (let item of cart.items) {
+        const product = await productsRepo.getOne(item.id);
+
+        item.product = product;
+    }
+
+    res.send(cartShowTemplate({ items: cart.items }));
+});
+
+router.post('/cart/products/delete', async (req, res) => {
+    const { itemId } = req.body;
+    const cart = await cartsRepo.getOne(req.session.cartId);
+
+    const items = cart.items.filter(item => item.id !== itemId);
+
+    await cartsRepo.update(req.session.cartId, { items });
+
+    res.redirect('/cart');
+});
 
 module.exports = router;
