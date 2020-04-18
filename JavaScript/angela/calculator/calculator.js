386cdca23bd08ebb7c0d8a3f491c2036b8766b59
Jossic <jossic.lapierre@gmail.com>
2020-04-18 21:39:53 +0200

    fin calculator

--- JavaScript/angela/calculator/calculator.js
+++ JavaScript/angela/calculator/calculator.js
@@ -1,9 +1,18 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser');
 
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+
+    const result = Number(req.body.num1) + Number(req.body.num2);
+    res.send(`The result is : ${result} !`);
 });
 
 
