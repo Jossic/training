5edb9b05b3891ee6ef0a9bbcfbc97feca18cdffd
Jossic <jossic.lapierre@gmail.com>
2020-04-19 21:10:50 +0200

    fin newsletter

--- JavaScript/angela/signupNews/app.js
+++ JavaScript/angela/signupNews/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = ('request');
+const request = require('request');
+const https = require('https');
 
 const app = express();
 
@@ -11,19 +12,59 @@ app.get('/signup', (req, res) => {
     res.sendFile(__dirname + '/signup.html');
 });
 
-app.get('/signup', (req, res) => {
-    res.send('Bonjour');
-});
 
 app.post('/signup', (req, res) => {
     const prenom = req.body.prenom;
     const mail = req.body.mail;
-    console.log(prenom, mail);
+    const data = {
+        members: [
+            {
+                email_address: mail,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: prenom,
+                }
+            }]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+
+    const url = 'https://us4.api.mailchimp.com/3.0/lists/c934f56af0';
+
+    const options = {
+        method: "POST",
+        auth: "Jossic:37734945172bb29746e94687675fd8a5-us4"
+    };
+
+    const request = https.request(url, options, (response) => {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + '/success.html');
+        } else {
+            res.sendFile(__dirname + '/failure.html');
+        }
+
+        response.on('data', (data) => {
+            console.log(JSON.parse(data));
+        })
+
+
+    });
+    request.write(jsonData);
+    request.end();
+
 });
 
+
+app.post('/failure', (req, res) => {
+    res.redirect('/signup');
+});
 
 
 
 app.listen(3000, () => {
     console.log("Listening")
 });
+
+
