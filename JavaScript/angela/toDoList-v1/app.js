7414baed94adcf22d55c71c094aecc1591084395
Jossic <jossic.lapierre@gmail.com>
2020-04-20 20:52:44 +0200

    fin 281

--- JavaScript/angela/toDoList-v1/app.js
+++ JavaScript/angela/toDoList-v1/app.js
@@ -4,7 +4,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const items = ["num 1", "num 2", "num 3"];
+const workItems = "";
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 
@@ -16,10 +22,20 @@ app.get('/', (req, res) => {
     var date = today.toLocaleDateString('fr-FR', options);
     console.log(date);
 
-    res.render('list', { date });
+    res.render('list', { listTitle, items });
 
 });
 
+app.post('/', (req, res) => {
+    const item = req.body.newItem;
+
+    items.push(item);
+    res.redirect('/');
+});
+
+app.get('/work', (req, res) => {
+    res.render('list', { listTitle: "Work List", items: workItems });
+});
 
 app.listen(3000, () => {
     console.log('Listening...');
