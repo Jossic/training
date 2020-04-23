3b8fa2d8d64a1e007391206ccf5d8c67564d85c5
Jossic <jossic.lapierre@gmail.com>
2020-04-23 22:31:04 +0200

    fin 23/04/2020

--- JavaScript/angela/ejs-challenge/app.js
+++ JavaScript/angela/ejs-challenge/app.js
@@ -3,8 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require('lodash');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,14 +11,12 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-mongoose.connect('mongodb://localhost:27017/blogDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const postSchema = {
   title: String,
@@ -28,85 +25,54 @@ const postSchema = {
 
 const Post = mongoose.model("Post", postSchema);
 
-
-
-
-app.get('/', (req, res) => {
+app.get("/", function (req, res) {
 
   Post.find({}, function (err, posts) {
-
     res.render("home", {
-
       startingContent: homeStartingContent,
-
       posts: posts
-
     });
-
-  })
-
+  });
 });
 
-app.get('/about', (req, res) => {
-  res.render('About', { aboutContent });
-
+app.get("/compose", function (req, res) {
+  res.render("compose");
 });
 
-app.get('/contact', (req, res) => {
-  res.render('Contact', { contactContent });
-
-});
-
-app.get('/compose', (req, res) => {
-  res.render('compose');
-
-});
-
-app.post('/compose', (req, res) => {
-
-
-
+app.post("/compose", function (req, res) {
   const post = new Post({
-    title: req.body.title,
-    content: req.body.content
+    title: req.body.postTitle,
+    content: req.body.postBody
   });
+
+
   post.save(function (err) {
-
     if (!err) {
-
       res.redirect("/");
-
     }
-
   });
-
-  res.redirect('/');
-
 });
 
-app.get('/posts/:postId', (req, res) => {
+app.get("/posts/:postId", function (req, res) {
 
-  const reqPost = _.lowerCase(req.params.postId);
+  const requestedPostId = req.params.postId;
 
   Post.findOne({ _id: requestedPostId }, function (err, post) {
-
     res.render("post", {
-
       title: post.title,
-
       content: post.content
-
     });
   });
+
 });
 
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
 
-
-
-
-
-
-
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
 
 
 app.listen(3000, function () {
