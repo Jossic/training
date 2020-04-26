2bfe821f0b6247a926a67a379d4384816c216ed4
Jossic <jossic.lapierre@gmail.com>
2020-04-26 14:54:14 +0200

    fin 404

--- JavaScript/angela/secrets/app.js
+++ JavaScript/angela/secrets/app.js
@@ -1,11 +1,11 @@
 //jshint esversion:6
 require('dotenv').config()
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -41,33 +41,41 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+
+        newUser.save((err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets');
+            }
+        }
+        );
     });
 
-    newUser.save((err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('secrets');
-        }
-    }
-    );
+
+
 });
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({ email: username }, (err, foundUser) => {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render('secrets');
-                }
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result === true) {
+                        res.render('secrets');
+                    }
+                });
+
             }
         }
     });
