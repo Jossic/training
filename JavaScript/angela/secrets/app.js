a611c2db62fc892be0ff11f889593ce1b193e3df
Jossic <jossic.lapierre@gmail.com>
2020-04-26 10:29:53 +0200

    fin 402

--- JavaScript/angela/secrets/app.js
+++ JavaScript/angela/secrets/app.js
@@ -1,10 +1,11 @@
 //jshint esversion:6
 require('dotenv').config()
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -25,8 +26,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
-
 const User = new mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => {
@@ -44,7 +43,7 @@ app.get('/login', (req, res) => {
 app.post('/register', (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save((err) => {
@@ -59,7 +58,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username }, (err, foundUser) => {
         if (err) {
