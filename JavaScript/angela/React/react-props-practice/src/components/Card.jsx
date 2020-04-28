1be856b11ca2b241359a5adf5f3e6e90d1f55060
Jossic <jossic.lapierre@gmail.com>
2020-04-28 21:14:41 +0200

    init emoji

---
+++ JavaScript/angela/React/react-props-practice/src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Avatar from './Avatar';
+
+
+function Card(props) {
+    return (<div>
+
+        <div className="card">
+            <div className="top">
+                <h2 className="name">{props.name}</h2>
+                <Avatar img={props.img} />
+            </div>
+            <div className="bottom">
+                <p className="info">{props.tel}</p>
+                <p className="info">{props.email}</p>
+            </div>
+        </div>
+    </div>);
+}
+
+export default Card;
