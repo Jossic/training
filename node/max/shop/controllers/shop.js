0c76d22ee206a3a33a6d23075a21b3dd5eec7fbb
Jossic <jossic.lapierre@gmail.com>
2020-05-16 15:48:47 +0200

    fin 108

---
+++ node/max/shop/controllers/shop.js
@@ -0,0 +1,40 @@
+const Product = require('../models/product');
+
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll(products => {
+        res.render('shop/product-list', {
+            prods: products,
+            pageTitle: 'Products list',
+            path: '/products',
+        });
+    });
+};
+
+exports.getIndex = (req, res, next) => {
+    Product.fetchAll(products => {
+        res.render('shop/index', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+        });
+    });
+};
+
+exports.getCart = (req, res, next) => {
+    Product.fetchAll(products => {
+        res.render('shop/cart', {
+            pageTitle: 'Cart',
+            path: '/cart',
+        });
+    });
+};
+
+exports.getCheckout = (req, res, next) => {
+    Product.fetchAll(products => {
+        res.render('shop/checkout', {
+            pageTitle: 'Checkout',
+            path: '/checkout',
+        });
+    });
+};
--- node/max/shop/models/product.js
+++ node/max/shop/models/product.js
@@ -18,8 +18,11 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imgURL, description, price) {
+    this.title = title;
+    this.imgURL = imgURL;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
