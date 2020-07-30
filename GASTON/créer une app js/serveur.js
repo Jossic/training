66907e2654637158415da26596eeac817ef3307b
Jossic <jossic.lapierre@gmail.com>
2020-07-30 22:06:18 +0200

    fin etape 7

--- GASTON/créer une app js/serveur.js
+++ GASTON/créer une app js/serveur.js
@@ -1,10 +1,13 @@
 const personnages = require('./personnages.json');
 const express = require('Express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get(['/', '/index'], (req, res) => {
     res.render('index', { pseudo: "Jossic" });
@@ -19,6 +22,20 @@ app.get('/personnage', (req, res) => {
     res.render('personnage', { perso: personnages[idPerso] });
 })
 
+app.post('/personnage', (req, res) => {
+    const monPerso = req.body.perso;
+    const monArme = req.body.arme;
+
+    let perso = '';
+    for (perso in personnages) {
+        if (monPerso === personnages[perso].nom) {
+            personnages[perso].arme = monArme
+            break;
+        }
+    }
+    fs.writeFileSync('personnages.json', JSON.stringify(personnages, undefined, 4));
+    res.render('personnage', { perso: personnages[perso] });
+})
 
 
 
