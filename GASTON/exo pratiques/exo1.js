e40b3bb83300a5320be4679860bf94fdd750ad94
Jossic <jossic.lapierre@gmail.com>
2020-07-31 22:05:34 +0200

    fin exo 1

---
+++ GASTON/exo pratiques/exo1.js
@@ -0,0 +1,15 @@
+let a = 1;
+let b = 5;
+
+console.log("---------Avant----------");
+console.log("a :", a);
+console.log("b :", b);
+
+let temp = a;
+a = b;
+b = temp;
+
+
+console.log("---------Apres----------");
+console.log("a :", a);
+console.log("b :", b);
