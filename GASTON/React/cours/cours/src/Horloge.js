8989c774d1d6b77caadac9ca1097880958023b79
Jossic <jossic.lapierre@gmail.com>
2020-08-12 13:26:41 +0200

    fin setState

--- GASTON/React/cours/cours/src/Horloge.js
+++ GASTON/React/cours/cours/src/Horloge.js
@@ -8,10 +8,21 @@ export default class Horloge extends Component {
     // Ou
     state = {
         date: new Date(),
+        compteur: 1
+    }
+
+    tick = () => {
+        this.setState((oldState, props) => {
+            return {
+                date: new Date(),
+                compteur: oldState.compteur + 1
+            }
+        }
+        );
     }
 
     componentDidMount() {
-        this.timerID = setInterval(() => this.setState({ date: new Date() }), 1000);
+        this.timerID = setInterval(() => this.tick(), 1000);
     }
 
     componentWillUnmount() {
@@ -21,9 +32,10 @@ export default class Horloge extends Component {
 
     render() {
         return (
-            <h1>
-                Heure : {this.state.date.toLocaleTimeString()}
-            </h1>
+            <>
+                <h2>Heure : {this.state.date.toLocaleTimeString()}</h2>
+                <div>Compteur : {this.state.compteur}</div>
+            </>
         )
     }
 }
