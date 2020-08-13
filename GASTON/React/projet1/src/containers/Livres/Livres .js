05ce6895c65176a834570757bd8609ad3d568959
Jossic <jossic.lapierre@gmail.com>
2020-08-13 13:22:00 +0200

    Liste des livres

--- GASTON/React/projet1/src/containers/Livres/Livres .js
+++ GASTON/React/projet1/src/containers/Livres/Livres .js
@@ -3,6 +3,37 @@ import Button from '../../components/Buttons/Button';
 
 
 export default class Livres extends Component {
+    state = {
+        livres: [
+            {
+                id: 1,
+                titre: "Il suffit d'une rencontre pour changer de vie",
+                auteur: "Anto Nevo",
+                nbPages: 150
+            },
+            {
+                id: 2,
+                titre: "L'autoroute du millionnaire",
+                auteur: "Machin",
+                nbPages: 250
+            },
+            {
+                id: 3,
+                titre: "Javascript de A à Z",
+                auteur: "Bidule",
+                nbPages: 300
+            },
+            {
+                id: 4,
+                titre: "Il suffit d'une rencontre pour changer de vie 2",
+                auteur: "Anto Nevo",
+                nbPages: 200
+            },
+
+        ]
+        ,
+
+    }
     render() {
         return (
             <table className="table text-center">
@@ -14,14 +45,19 @@ export default class Livres extends Component {
                         <td colSpan="2">Actions</td>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr className="table-light">
-                        <td>Titre 1</td>
-                        <td>Auteur 1</td>
-                        <td>X pages</td>
-                        <td><Button type="btn-info" clic={() => console.log('Modification')}>Modifier</Button></td>
-                        <td><Button type="btn-warning" clic={() => console.log('Suppression')}>Supprimer</Button></td>
-                    </tr>
+                <tbody className="table-light">
+                    {this.state.livres.map((livre) => {
+                        return (
+                            <tr key={livre.id}>
+                                <td>{livre.titre}</td>
+                                <td>{livre.auteur}</td>
+                                <td>{livre.nbPages}</td>
+                                <td><Button type="btn-info" clic={() => console.log('Modification')}>Modifier</Button></td>
+                                <td><Button type="btn-warning" clic={() => console.log('Suppression')}>Supprimer</Button></td>
+                            </tr>
+                        )
+                    })}
+
                 </tbody>
             </table>
         )
