2410d89b399c2e30415cd2c32734ec44f2dfbc59
Jossic <jossic.lapierre@gmail.com>
2020-08-15 10:18:37 +0200

    fin création ajout formulaire

--- GASTON/React/projet1/src/containers/Livres/FormulaireAjout/FormulaireAjout.js
+++ GASTON/React/projet1/src/containers/Livres/FormulaireAjout/FormulaireAjout.js
@@ -1,6 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Button from '../../../components/Buttons/Button';
 
 export default class FormulaireAjout extends Component {
+    state = {
+        monTitre: "",
+        monAuteur: "",
+        mesPages: ""
+    }
     render() {
         return (
             <>
@@ -8,9 +14,17 @@ export default class FormulaireAjout extends Component {
                 <form>
                     <div className="form-group">
                         <label for="titre">Titre du livre</label>
-                        <input type="text" className="form-control" id="titre" placeholder="Titre du livre" />
+                        <input type="text" value={this.state.monTitre} className="form-control" id="titre" placeholder="Titre du livre" onChange={e => this.setState({ monTitre: e.target.value })} />
                     </div>
-                    <button type="submit" className="btn btn-primary">Envoyer</button>
+                    <div className="form-group">
+                        <label for="auteur">Auteur du livre</label>
+                        <input type="text" value={this.state.monAuteur} className="form-control" id="auteur" placeholder="Auteur du livre" onChange={e => this.setState({ monAuteur: e.target.value })} />
+                    </div>
+                    <div className="form-group">
+                        <label for="nbPages">Nombre de pages du livre</label>
+                        <input type="text" value={this.state.mesPages} className="form-control" id="nbPages" placeholder="Nombre de pages du livre" onChange={e => this.setState({ mesPages: e.target.value })} />
+                    </div>
+                    <Button type="btn-primary">Envoyer</Button>
                 </form>
             </>
         )
