07b536c33a1b6e61cc5ca2d4b714e777bdea8f12
Jossic <jossic.lapierre@gmail.com>
2020-08-20 14:08:30 +0200

    fin navbar

--- GASTON/React/projet3/src/App.js
+++ GASTON/React/projet3/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import PaysManager from './containers/PaysManager/PaysManager';
+import NavBar from './components/NavBar/NavBar';
+import NavBar2 from './components/NavBar/NavBar2';
 
 function App() {
   return (
     <BrowserRouter>
-      <Link to="/">Accueil</Link>
-      <Link to="/pays">Pays</Link>
+      <Route path='/pays' exact component={NavBar} />
+      <Route path='/admin' component={NavBar2} />
       <Route path='/' exact render={() => <h1>Page d'accueil</h1>} />
-      <Route path='/pays' component={PaysManager} />
+      <Route path='/pays' exact component={PaysManager} />
+      <Route path='/admin' exact render={() => <h1>Page d'administration</h1>} />
 
     </BrowserRouter>
   );
---
+++ GASTON/React/projet3/src/components/NavBar/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = (props) => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <Link to="/" exact className="nav-link">Accueil</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/pays" exact className="nav-link">Pays</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    );
+
+}
+
+export default NavBar;
