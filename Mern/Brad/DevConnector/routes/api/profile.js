bb273e1de36f31ba809eb18a6c9826c257f6e398
Jossic <jossic.lapierre@gmail.com>
2020-08-24 21:51:13 +0200

    fin 21

--- Mern/Brad/DevConnector/routes/api/profile.js
+++ Mern/Brad/DevConnector/routes/api/profile.js
@@ -200,5 +200,25 @@ router.put('/experience', [auth, [
     }
 })
 
+// @route    DELETE api/profile/experience/:exp_id
+// @desc     Delete profile exp
+// @access   Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+    } catch (e) {
+        console.log(e.message);
+        res.status(500).send('Erreur Serveur');
+    }
+});
+
 
 module.exports = router;
