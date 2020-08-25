4b4235e869092415aad65f39b6eaee2506f44196
Jossic <jossic.lapierre@gmail.com>
2020-08-25 21:30:12 +0200

    fin 23

--- Mern/Brad/DevConnector/routes/api/profile.js
+++ Mern/Brad/DevConnector/routes/api/profile.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { route } = require('./users');
 const router = express.Router();
+const request = require('request');
+const config = require('config');
 const auth = require('../../middleware/auth');
 const { body, validationResult } = require('express-validator');
 
@@ -286,6 +288,32 @@ router.delete('/formation/:formation_id', auth, async (req, res) => {
         res.status(500).send('Erreur Serveur');
     }
 });
+
+// @route    GET api/profile/github/:username
+// @desc     Get user reps from github
+// @access   Public
+router.get('/github/:username', (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: { 'user-agent': "node.js" }
+        };
+
+        request(options, (error, response, body) => {
+            if (error) {
+                console.log(error)
+            }
+            if (response.statusCode !== 200) {
+                return res.status(404).json({ msg: "Aucun profil  Github trouvé" });
+            }
+            res.json(JSON.parse(body));
+        });
+    } catch (e) {
+        console.log(e.message);
+        res.status(500).send('Erreur Serveur');
+    }
+})
 
 
 module.exports = router;
