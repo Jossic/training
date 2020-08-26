2caf752fd85a2a54858f35550f0b9041deb85b32
Jossic <jossic.lapierre@gmail.com>
2020-08-26 22:31:14 +0200

    fin 27

--- Mern/Brad/DevConnector/routes/api/posts.js
+++ Mern/Brad/DevConnector/routes/api/posts.js
@@ -101,4 +101,49 @@ router.delete('/:id', auth, async (req, res) => {
     }
 });
 
+// @route     PUT api/posts/like/:id
+// @desc      Like a post
+// @access    Private
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ msg: 'Ce post est déjà like' });
+        }
+
+        post.likes.unshift({ user: req.user.id });
+
+        await post.save();
+        res.json(post.likes);
+    } catch (e) {
+        console.log(e.message);
+        res.status(500).send('Erreur serveur');
+    }
+});
+
+// @route     PUT api/posts/unlike/:id
+// @desc      Unlike a post
+// @access    Private
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({ msg: 'Ce post n\'est pas déjà like' });
+        }
+
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+        post.likes.splice(removeIndex, 1);
+
+        await post.save();
+
+        res.json(post.likes);
+    } catch (e) {
+        console.log(e.message);
+        res.status(500).send('Erreur serveur');
+    }
+});
+
 module.exports = router;
