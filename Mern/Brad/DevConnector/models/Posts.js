8960dfd9f73fc080561398e280f0362c8d6af5e6
Jossic <jossic.lapierre@gmail.com>
2020-08-26 21:33:42 +0200

    fin 25

--- Mern/Brad/DevConnector/models/Posts.js
+++ Mern/Brad/DevConnector/models/Posts.js
@@ -5,7 +5,7 @@ const PostSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'users'
     },
-    text: {
+    texte: {
         type: String,
         required: true,
     },
--- Mern/Brad/DevConnector/routes/api/posts.js
+++ Mern/Brad/DevConnector/routes/api/posts.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
+const Post = require('../../models/Posts');
+const User = require('../../models/User');
 
-router.get('/', (req, res) => {
-    res.send('Posts route');
-})
+// @route     POST api/posts
+// @desc      Create a post
+// @access    Private
+router.get('/', [auth, [
+    body('texte', "Le texte est recquis").not().isEmpty()
+]],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+
+            const newPost = new Post({
+                texte: req.body.texte,
+                nom: user.nom,
+                avatar: user.avatar,
+                user: req.user.id
+            });
+
+            const post = await newPost.save();
+
+            res.json(post);
+        } catch (e) {
+            console.log(e);
+            res.status(500).send('Erreur serveur');
+        }
+
+    });
 
 module.exports = router;
