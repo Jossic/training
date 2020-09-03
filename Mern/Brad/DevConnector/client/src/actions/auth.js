feafdebd4a062a349193aad3581d8874913bb02f
Jossic <jossic.lapierre@gmail.com>
2020-09-03 22:05:44 +0200

    fin 43

--- Mern/Brad/DevConnector/client/src/actions/auth.js
+++ Mern/Brad/DevConnector/client/src/actions/auth.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL } from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from './types';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -84,3 +84,7 @@ export const login = (email, password) => async dispatch => {
         })
     }
 }
+
+export const logout = () => dispatch => {
+    dispatch({ type: LOGOUT });
+};
