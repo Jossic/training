c289afcb0184f6052c30dfaf1c5fa5a921e59f80
Jossic <jossic.lapierre@gmail.com>
2020-09-05 22:28:15 +0200

    fin 47

--- Mern/Brad/DevConnector/client/src/components/dashboard/Dashboard.js
+++ Mern/Brad/DevConnector/client/src/components/dashboard/Dashboard.js
@@ -1,19 +1,29 @@
 import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import Spinner from '../layout/Spinner'
 import { getCurrentProfile } from '../../actions/profile'
+import { Link } from 'react-router-dom'
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
     useEffect(() => {
         getCurrentProfile();
 
     }, []);
 
-    return (
-        <>
-            Accuel dashboard
-        </>
-    )
+    return loading && profile === null ? <Spinner /> : <>
+        <h1 className="large text-primary">
+            Dashboard
+        </h1>
+        <p className="lead">
+            <i className="fas fas-user"> Bienvenue {user && user.name} </i>
+        </p>
+        {profile !== null ?
+            <p>has</p> :
+            <p>Votre profil n'est pas encore créé, merci d'en créer un.<br />
+                <Link to='/create-profile' className='btn btn-primary my-1'> Créer un profil</Link>
+            </p>}
+    </>;
 }
 
 Dashboard.propTypes = {
