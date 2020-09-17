f4b324deb07ad75ee01d17bef8594c54956aae5b
Jossic <jossic.lapierre@gmail.com>
2020-09-17 13:22:04 +0200

    init projet github-finder

---
+++ react/weather/src/components/weathercard/WeatherCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const WeatherCard = (props) => {
+    return (
+        <div className='card'>
+            <h2>Sydney</h2>
+            <h3>AU</h3>
+            <img src="" alt="Weather" />
+            <h2>20°C</h2>
+            <h3>Cloudy</h3>
+        </div>
+    )
+}
