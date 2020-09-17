2729ea91d41190d692cfe6f5ec3b3db7f9966db2
Jossic <jossic.lapierre@gmail.com>
2020-09-17 21:56:20 +0200

    fin 15 - ajout spinner

--- react/Brad/github-finder/src/App.js
+++ react/Brad/github-finder/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import Users from './components/layout/users/Users';
+import Users from './components/users/Users';
 import axios from 'axios'
 
 class App extends Component {
