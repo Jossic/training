93c6b37c4fe68d6c817d6f495bf7fb6c12890a2f
Jossic <jossic.lapierre@gmail.com>
2020-09-18 15:01:10 +0200

    fin 22 - afficher page d'un util

--- react/Brad/github-finder/src/App.js
+++ react/Brad/github-finder/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import axios from 'axios'
 import Alert from './components/layout/Alert';
@@ -12,6 +13,7 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   }
@@ -31,6 +33,14 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  getUser = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ user: res.data, loading: false });
+  }
+
   clearUsers = () => {
     this.setState({ users: [], loading: false });
   };
@@ -43,7 +53,7 @@ class App extends Component {
   }
 
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, user } = this.state;
     return (
       <Router>
         <div className="App">
@@ -63,6 +73,9 @@ class App extends Component {
                 </>
               )} />
               <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' render={props => (
+                <User {...props} getUser={this.getUser} user={user} loading={loading} />
+              )} />
             </Switch>
 
           </div>
--- react/Brad/github-finder/src/components/users/UserItem.js
+++ react/Brad/github-finder/src/components/users/UserItem.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
 
@@ -8,7 +10,7 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
             <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }} />
             <h3>{login}</h3>
             <div>
-                <a href={html_url} className="btn btn-dark btn-sm my-1">Plus d'infos</a>
+                <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">Plus d'infos</Link>
             </div>
         </div>
     )
