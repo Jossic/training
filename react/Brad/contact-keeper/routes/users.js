3d4a4afebe04a4dc497264e888baa39608b330ad
Jossic <jossic.lapierre@gmail.com>
2020-09-19 21:25:22 +0200

    Contact Keeper - 42 - User Model & Validation

--- react/Brad/contact-keeper/routes/users.js
+++ react/Brad/contact-keeper/routes/users.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @route   POST  api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-    res.send('Enregistrement d\'un utilisateur')
+router.post('/', [
+    body('name', 'Merci de renseigner votre nom').not().isEmpty(),
+    body('email', 'Merci de renseigner un email correct').isEmail(),
+    body('password', 'Merci de renseigner un mot de passe correspondant').isLength({
+        min: 6
+    }),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('ok');
 });
 
 
