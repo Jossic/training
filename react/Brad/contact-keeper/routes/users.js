269fd320389911ab89ea7b5902f0fb54b131bec4
Jossic <jossic.lapierre@gmail.com>
2020-09-19 21:33:25 +0200

    Contact Keeper - 43 - Hash Passwords & Register Route

--- react/Brad/contact-keeper/routes/users.js
+++ react/Brad/contact-keeper/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 
@@ -13,13 +14,39 @@ router.post('/', [
     body('password', 'Merci de renseigner un mot de passe correspondant').isLength({
         min: 6
     }),
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('ok');
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({ msg: "Cet utilisateur existe déjà" });
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.send('Utilisateur enregistré');
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erreur Serveur');
+    }
 });
 
 
