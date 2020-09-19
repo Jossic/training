8278550a3495330d1935f101213dc9284c782134
Jossic <jossic.lapierre@gmail.com>
2020-09-19 14:44:46 +0200

    fin 34 - Home & NotFound component

--- react/Brad/github-finder/src/App.js
+++ react/Brad/github-finder/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
+import Home from './components/pages/Home';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+
 
 import './App.css';
 
@@ -23,14 +24,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route exact path='/' render={props => (
-                  <>
-                    <Search />
-                    <Users />
-                  </>
-                )} />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
 
             </div>
---
+++ react/Brad/github-finder/src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Page non trouvée</h1>
+            <p className="lead">Cette page n'existe pas...</p>
+        </div>
+    )
+}
+
+export default NotFound
