f54551f800bd9f7df2d549228fc188beb543f7eb
Jossic <jossic.lapierre@gmail.com>
2020-09-19 16:04:48 +0200

    Contact Keeper - Init BackEnd

---
+++ react/Brad/contact-keeper/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ msg: 'hello woooorld' })
+})
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
+
