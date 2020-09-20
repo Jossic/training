d1aea57a1e10907664fc6e3f3734b0993fe9b0e1
Jossic <jossic.lapierre@gmail.com>
2020-09-20 22:22:47 +0200

    Contact Keeper - 51 - Cleanup, Navbar & Router Setup

---
+++ react/Brad/contact-keeper/client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h2>A propos de cette App</h2>
+            <p className="my-1">C'est une App de gestion de contacts réalisé avec le MERN Stack</p>
+            <p className="bg-dark p">
+                <strong>Version</strong> 1.0.0
+           </p>
+        </div>
+    )
+}
+
+export default About
