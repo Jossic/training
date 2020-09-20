22cdcfa4679cb3e48718e9f889c82b9cc8e6fc87
Jossic <jossic.lapierre@gmail.com>
2020-09-20 13:41:03 +0200

    Contact Keeper - 47 - Contact Model & Get Contacts Route

--- react/Brad/contact-keeper/routes/contacts.js
+++ react/Brad/contact-keeper/routes/contacts.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { body, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route   GET  api/contacts
 // @desc    Get all users contacts
 // @access  Private
-router.get('/', (req, res) => {
-    res.send('Récupérer tous les contacts')
+router.get('/', auth, async (req, res) => {
+    try {
+        const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1 })
+        res.json(contacts);
+    } catch (e) {
+        console.error(e.message);
+        res.status(500).send('Erreur serveur')
+    }
 });
 
 // @route   POST  api/contacts
