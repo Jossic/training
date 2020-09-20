233702c7310d248fadd8a92572a2231aba690444
Jossic <jossic.lapierre@gmail.com>
2020-09-20 13:51:50 +0200

    Contact Keeper - 48 - Add Contact Route

--- react/Brad/contact-keeper/routes/contacts.js
+++ react/Brad/contact-keeper/routes/contacts.js
@@ -22,8 +22,32 @@ router.get('/', auth, async (req, res) => {
 // @route   POST  api/contacts
 // @desc    Add new contacts
 // @access  Private
-router.post('/', (req, res) => {
-    res.send('Ajout d\'un contacts')
+router.post('/', [auth, [
+    body('name', 'Merci de renseigner un nom').not().isEmpty(),
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+        const newContact = new Contact({
+            name,
+            email,
+            phone,
+            type,
+            user: req.user.id
+        });
+
+        const contact = await newContact.save();
+
+        res.json(contact);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erreur Serveur');
+    }
 });
 
 // @route   PUT  api/contacts/:id
