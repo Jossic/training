b0d2069a920d58c77d60acaf21b047a7269a43d9
Jossic <jossic.lapierre@gmail.com>
2020-09-23 22:24:16 +0200

    Contact Keeper - 57 - Edit & Update Contact Action

--- react/Brad/contact-keeper/client/src/components/contacts/ContactForm.js
+++ react/Brad/contact-keeper/client/src/components/contacts/ContactForm.js
@@ -1,8 +1,23 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import ContactContext from '../../context/contact/contactContext';
 
 const ContactForm = () => {
     const contactContext = useContext(ContactContext);
+
+    const { addContact, updateContact, clearCurrent, current } = contactContext;
+
+    useEffect(() => {
+        if (current !== null) {
+            setContact(current);
+        } else {
+            setContact({
+                name: '',
+                email: '',
+                phone: '',
+                type: 'personal'
+            });
+        }
+    }, [contactContext, current])
 
     const [contact, setContact] = useState({
         name: '',
@@ -17,18 +32,23 @@ const ContactForm = () => {
 
     const onSubmit = e => {
         e.preventDefault();
-        contactContext.addContact(contact);
-        setContact({
-            name: '',
-            email: '',
-            phone: '',
-            type: 'personal'
-        })
+        if (current === null) {
+            addContact(contact);
+        } else {
+            updateContact(contact);
+        }
+        clearAll();
+    }
+
+    const clearAll = () => {
+        clearCurrent();
     }
 
     return (
         <form onSubmit={onSubmit}>
-            <h2 className="text-primary">Ajouter un contact</h2>
+            <h2 className="text-primary">
+                {current ? 'Modifier un contact' : 'Ajouter un contact'}
+            </h2>
             <input
                 type="text"
                 placeholder='Nom'
@@ -51,7 +71,10 @@ const ContactForm = () => {
             <input type="radio" name="type" value="personal" checked={type === 'personal'} onChange={onChange} /> Perso {' '}
             <input type="radio" name="type" value="professional" checked={type === 'professional'}
                 onChange={onChange} /> Pro
-            <div><input type="submit" className='btn btn-primary btn-block' value="Ajouter un contact" /></div>
+            <div><input type="submit" className='btn btn-primary btn-block' value={current ? 'Modifier ce contact' : 'Ajouter ce contact'} /></div>
+            {current && <div>
+                <button className='btn btn-light btn-block' onClick={clearAll}>Reset</button>
+            </div>}
         </form>
     )
 }
--- react/Brad/contact-keeper/client/src/context/contact/ContactState.js
+++ react/Brad/contact-keeper/client/src/context/contact/ContactState.js
@@ -63,7 +63,9 @@ const ContactState = props => {
     }
 
     // Update contact
-
+    const updateContact = (contact) => {
+        dispatch({ type: UPDATE_CONTACT, payload: contact });
+    }
     // Filter contacts
 
     // Clear filter
@@ -76,7 +78,8 @@ const ContactState = props => {
                 addContact,
                 deleteContact,
                 setCurrent,
-                clearCurrent
+                clearCurrent,
+                updateContact
             }}
         >
             {props.children}
