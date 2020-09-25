52a1f4b4b8580b35b45aa7c5dcd075fc0e4ec382
Jossic <jossic.lapierre@gmail.com>
2020-09-25 21:38:44 +0200

    Contact Keeper - 62 - Alert Context, State & Component

--- react/Brad/contact-keeper/client/src/components/auth/Register.js
+++ react/Brad/contact-keeper/client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import AlertContext from '../../context/alert/alertContext'
 
 const Register = () => {
+    const alertContext = useContext(AlertContext);
+
+    const { setAlert } = alertContext;
+
     const [user, setUser] = useState({
         name: '',
         email: '',
@@ -14,6 +19,15 @@ const Register = () => {
 
     const onSubmit = e => {
         e.preventDefault();
+        if (name === '' || email === '' || password === '') {
+            setAlert('Merci de renseigner tous les champs', 'danger')
+        } else if (password !== password2) {
+            setAlert('Les mots de passes ne correspondent pas', 'danger')
+        }
+        else {
+            console.log('success');
+        }
+
     }
 
     return (
@@ -24,19 +38,19 @@ const Register = () => {
             <form onSubmit={onSubmit}>
                 <div className="form-group">
                     <label htmlFor="name">Nom</label>
-                    <input type="text" name="name" value={name} onChange={onChange} />
+                    <input type="text" name="name" value={name} onChange={onChange} required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" value={email} onChange={onChange} />
+                    <input type="email" name="email" value={email} onChange={onChange} required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Mot de passe</label>
-                    <input type="password" name="password" value={password} onChange={onChange} />
+                    <input type="password" name="password" value={password} onChange={onChange} required minLength='6' />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password2">Confirmer le mot de passe</label>
-                    <input type="password" name="password2" value={password2} onChange={onChange} />
+                    <input type="password" name="password2" value={password2} onChange={onChange} required minLength='6' />
                 </div>
                 <input type="submit" value="Register" className='btn btn-primary btn-block' />
             </form>
